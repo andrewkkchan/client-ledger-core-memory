6d43a30a1eab86689f398a12bb4fcc0c566d0aeb
Andrew Chan <andrew.koonkit.chan@gmail.com>
2019-09-21 19:58:21 +0800

    fix

--- src/main/java/com/industrieit/ledger/clientledger/core/memory/config/LedgerConfig.java
+++ src/main/java/com/industrieit/ledger/clientledger/core/memory/config/LedgerConfig.java
@@ -39,7 +39,7 @@ public class LedgerConfig {
      */
     @Bean
     public Logger logger() {
-        return LoggerFactory.getLogger("com.industrieit.dragon.clientledger.core.redis");
+        return LoggerFactory.getLogger("com.industrieit.dragon.clientledger.core.memory");
     }
 
     /**
@@ -72,8 +72,8 @@ public class LedgerConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        config.put(ConsumerConfig.CLIENT_ID_CONFIG, "consumer_core_redis");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_core_redis");
+        config.put(ConsumerConfig.CLIENT_ID_CONFIG, "consumer_core_memory");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_core_memory");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config);
